e9149d09554eac2e6fdc0413d336b3d76a82a57e
Dmedina88 <dynamitemedina@gmail.com>
2015-04-04 04:20:07 -0400

    added interface that may be used for state idea
    added interface

---
+++ app/src/main/java/com/grayherring/nyc_opendata/interfaces/SearchStateInterface.java
@@ -0,0 +1,14 @@
+package com.grayherring.nyc_opendata.interfaces;
+
+/**
+ * Created by david on 4/4/2015.
+ */
+
+//  future plan witht his  uses state patern when clicking the button that got it to that state change state back to   as ifit was not pushed
+    // chang text  on buttons to match  if using acton button change   likly to be done in  an abstact class consuctor?
+public interface SearchStateInterface {
+    public void updateUI();
+    public void queriedSearchClicked();
+    public void locationalSearchClicked();
+    public void Search();
+}
